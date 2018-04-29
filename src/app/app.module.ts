d83548e12f2bbf8dce639ac5629e5e9e1c1640b3
anavoeg <dinizgeovana@hotmail.com>
2018-04-29 07:01:52 -0300

    primeiro commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { ServicoComponent } from './servico/servico.component';
+import { ContatoComponent } from './contato/contato.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ServicoComponent,
+    ContatoComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
